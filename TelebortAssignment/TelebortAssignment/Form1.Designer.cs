5ae574eaa1ba0f5503c17f252442b6d46cac5d19
danielleo96 <62012314+danielleo96@users.noreply.github.com>
2020-03-10 17:55:31 +0800

    Add files via upload

---
+++ TelebortAssignment/TelebortAssignment/Form1.Designer.cs
@@ -0,0 +1,172 @@
+﻿namespace TelebortAssignment
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.TimerLabel = new System.Windows.Forms.Label();
+            this.ScoreLabel = new System.Windows.Forms.Label();
+            this.Points = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.WordLabel = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(316, 191);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(149, 42);
+            this.button1.TabIndex = 0;
+            this.button1.TabStop = false;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.button1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.button1_KeyDown);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(213, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(140, 29);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Instructions:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(117, 61);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(513, 25);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Type as as many words as you can until the time runs out!";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(117, 113);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(77, 29);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Timer";
+            // 
+            // TimerLabel
+            // 
+            this.TimerLabel.AutoSize = true;
+            this.TimerLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TimerLabel.Location = new System.Drawing.Point(136, 144);
+            this.TimerLabel.Name = "TimerLabel";
+            this.TimerLabel.Size = new System.Drawing.Size(39, 29);
+            this.TimerLabel.TabIndex = 4;
+            this.TimerLabel.Text = "20";
+            // 
+            // ScoreLabel
+            // 
+            this.ScoreLabel.AutoSize = true;
+            this.ScoreLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ScoreLabel.Location = new System.Drawing.Point(601, 113);
+            this.ScoreLabel.Name = "ScoreLabel";
+            this.ScoreLabel.Size = new System.Drawing.Size(77, 29);
+            this.ScoreLabel.TabIndex = 5;
+            this.ScoreLabel.Text = "Score";
+            // 
+            // Points
+            // 
+            this.Points.AutoSize = true;
+            this.Points.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Points.Location = new System.Drawing.Point(626, 144);
+            this.Points.Name = "Points";
+            this.Points.Size = new System.Drawing.Size(26, 29);
+            this.Points.TabIndex = 6;
+            this.Points.Text = "0";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // WordLabel
+            // 
+            this.WordLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.WordLabel.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.WordLabel.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.WordLabel.Enabled = false;
+            this.WordLabel.Font = new System.Drawing.Font("Courier New", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.WordLabel.Location = new System.Drawing.Point(122, 265);
+            this.WordLabel.Name = "WordLabel";
+            this.WordLabel.ReadOnly = true;
+            this.WordLabel.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.None;
+            this.WordLabel.Size = new System.Drawing.Size(556, 96);
+            this.WordLabel.TabIndex = 8;
+            this.WordLabel.Text = "Words";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.WordLabel);
+            this.Controls.Add(this.Points);
+            this.Controls.Add(this.ScoreLabel);
+            this.Controls.Add(this.TimerLabel);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Typing For Cool Kids";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label TimerLabel;
+        private System.Windows.Forms.Label ScoreLabel;
+        private System.Windows.Forms.Label Points;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.RichTextBox WordLabel;
+    }
+}
+
---
+++ TelebortAssignment/TelebortAssignment/Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Timers;
+using System.IO;
+using System.Reflection;
+
+namespace TelebortAssignment
+{
+    public partial class Form1 : Form
+    {
+        int start = 0;
+        int end = 1;
+        int score = 0;
+        
+        public Form1()
+        {
+            InitializeComponent();
+            WordLabel.Visible = false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Points.Text = "0";
+            TimerLabel.Text = "20"; //start from 20 seconds
+            timer1.Start();
+            playGame();
+            
+            
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            TimerLabel.Text = (int.Parse(TimerLabel.Text) - 1).ToString();
+            if (int.Parse(TimerLabel.Text) == 0)  //if the countdown reaches '0', we stop it
+            {
+                timer1.Stop();
+                
+                button1.Visible = true;
+                WordLabel.Text = "Game Over";
+                WordLabel.ForeColor = Color.Red;
+            }
+        }
+
+
+        private void button1_KeyDown(object sender, KeyEventArgs e)
+        {
+            string s = WordLabel.Text;
+            char[] charArray = s.ToCharArray();
+            
+                foreach (var letter in charArray)
+                {
+                    
+                    if (e.KeyCode.ToString().ToUpper() == charArray[start].ToString())
+                    {
+
+                        WordLabel.Select(0, end);
+                        WordLabel.SelectionBackColor = Color.Blue;
+                        if (start < charArray.Length - 1)
+                        {
+                            start++; // duplicate letters
+                            end++;
+                        break;  
+                        }
+                        else
+                        {
+                            score++;
+                            Points.Text = score.ToString();
+                            playGame();
+                        }
+
+                    }
+               
+                }
+            
+        }
+        private void playGame()
+        {
+            string file_name = @"WordsFolder\words.txt";
+            string[] files = File.ReadAllLines(file_name);
+
+            int index = new Random().Next(0, files.Length);
+            var name = files[index];
+            IList<string> stringList = files.ToList();
+
+            stringList.RemoveAt(index);
+            string s = files[index].ToUpper();
+
+            button1.Visible = false;
+            WordLabel.Visible = true;
+            WordLabel.Text = s;
+            WordLabel.ForeColor = Color.Black;
+            start = 0;
+            end = 1;
+        }
+
+    }
+}
